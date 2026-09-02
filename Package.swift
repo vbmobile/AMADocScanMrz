1f7c2eb31776e68efb41da05dcef8e082dd9504d
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-09-02 15:01:46 +0100

    Publish AMADocScanMrz 3.1.0 package manifest

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/vbmobile/AMADocModel", .upToNextMinor(from: "3.0.1")),
+        .package(url: "https://github.com/vbmobile/AMADocModel", .upToNextMinor(from: "3.1.0")),
         .package(url: "https://github.com/vbmobile/AMADocScanner", .upToNextMinor(from: "5.0.2")),
         .package(url: "https://github.com/vbmobile/AMANetworkClient", .upToNextMinor(from: "6.0.1"))
     ],
     targets: [
         .binaryTarget(
             name: "AMADocScanMrz",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrz/AMADocScanMrz-3.0.4.zip",
-            checksum: "20288877d94d2116d2366e79c5542f444adabfaf6c088529cd0bf1fcfd5a7fcb"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrz/AMADocScanMrz-3.1.0.zip",
+            checksum: "2cdf0d9c3ab4c782f4db568044d572fa8cba7ec22b9c87b56fd52b9a9f66d043"
         ),
         .target(
             name: "AMADocScanMrzWrapper",
